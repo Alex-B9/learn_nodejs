632a77374c3cf8f846dd3e544e74a24ea483cdee
alex-b9 <baesalex9@gmail.com>
2022-07-08 15:37:26 +0200

    update controllers/stuff, create thing

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -1,43 +1,33 @@
 const Thing = require('../models/thing');
-// const fs = require('fs');
+const fs = require('fs');
+const {json} = require("express");
 
 exports.createThing = (req, res, next) => {
-    const thingObject = JSON.parse(req.body.thing);
-    delete thingObject._id;
-    delete thingObject._userId;
+    const check = JSON.stringify(req.body);
+    const checkThing = JSON.parse(check);
+
+    delete checkThing._id;
+    delete checkThing.userId;
+
     const thing = new Thing({
-        ...thingObject,
-        userId: req.auth.userId,
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    });
-
-    thing.save()
-        .then(() => { res.status(201).json({message: 'Post saved successfully!'})})
-        .catch(error => { res.status(400).json( { error: error })})
-
-    // -------------------------------------------------------------------------------------------
-
-    // const thing = new Thing({
-    //         title: req.body.title,
-    //         description: req.body.description,
-    //         imageUrl: req.body.imageUrl,
-    //         price: req.body.price,
-    //         userId: req.body.userId
-    //     });
-    //     thing.save().then(
-    //         () => {
-    //             res.status(201).json({
-    //                 message: 'Post saved successfully!'
-    //             });
-    //         }
-    //     ).catch(
-    //         (error) => {
-    //             res.status(400).json({
-    //                 error: error
-    //             });
-    //         }
-    //     );
+            ...checkThing,
+            userId: req.auth.userId
+        });
+    thing.save().then(
+            () => {
+                res.status(201).json({
+                    message: 'Post saved successfully!'
+                });
+            }
+        ).catch(
+            (error) => {
+                res.status(400).json({
+                    error: error
+                });
+            }
+        );
 };
+
 
 exports.getOneThing = (req, res, next) => {
     Thing.findOne({
@@ -56,6 +46,22 @@ exports.getOneThing = (req, res, next) => {
 };
 
 exports.modifyThing = (req, res, next) => {
+    // const thingObject = req.file ? {
+    //     ...JSON.parse(req.body.thing)
+    // } : {...req.body};
+    //
+    // delete thingObject.userId;
+    // Thing.findOne({_id: req.params.id})
+    //     .then((thing) => {
+    //         if (thing.userId != req.auth.userId) {
+    //             res.status(401).json({ message: "Non-autorisé" })
+    //         } else {
+    //             thing.updateOne({ _id: req.params.id }, {...thingObject, _id: req.params.id})
+    //                 .then(() => res.status(200).json({ message:"Objet modifié !" }))
+    //                 .catch(error => res.status(401).json({ error }));
+    //         }
+    //     })
+
     const thing = new Thing({
         _id: req.params.id,
         title: req.body.title,
